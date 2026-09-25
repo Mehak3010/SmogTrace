1c4f58c1488b7dacfcb197f3a0dfd5db3e9b45a0
Mehak Arora <mehak3010arora@gmail.com>
2026-09-25 11:57:20 +0530

    Delete stubble-dashboard-app directory

--- stubble-dashboard-app/src/pages/GeographyPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import GeoModelPage from './GeoModelPage';
-
-export default function GeographyPage() {
-  return <GeoModelPage />;
-}
--- stubble-dashboard-app/src/pages/OverviewPage.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import SignalPage from './SignalPage';
-
-export default function OverviewPage() {
-  return <SignalPage />;
-}
